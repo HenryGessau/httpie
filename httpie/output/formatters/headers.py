afdfb846bd2dce7969d8452733374106a50ef1df
Henry Gessau <HenryG@gessau.net>
2019-05-12 18:27:30 -0400

    Quash boring headers

--- httpie/output/formatters/headers.py
+++ httpie/output/formatters/headers.py
@@ -1,6 +1,38 @@
 from httpie.plugins import FormatterPlugin
 
 
+QUASHED = [
+
+    # Request
+    'Accept',
+    'Accept-Encoding',
+    'Authorization',
+    'Connection',
+    'Host',
+    'User-Agent',
+
+    # Response
+    'Connection',
+    'Cache-Control',
+    'Content-Encoding',
+    'Content-Security-Policy',
+    'Content-Type',
+    # 'Date',
+    'Expect-CT',
+    'Expires',
+    'Pragma',
+    'Referrer-Policy',
+    'Server',
+    'Set-Cookie',
+    'Strict-Transport-Security',
+    'Transaction-Id',
+    'Transfer-Encoding',
+    'X-Content-Type-Options',
+    'X-Frame-Options',
+    'X-XSS-Protection',
+]
+
+
 class HeadersFormatter(FormatterPlugin):
 
     def format_headers(self, headers: str) -> str:
@@ -11,4 +43,5 @@ class HeadersFormatter(FormatterPlugin):
         """
         lines = headers.splitlines()
         headers = sorted(lines[1:], key=lambda h: h.split(':')[0])
+        headers = [h for h in headers if h.split(':')[0] not in QUASHED]
         return '\r\n'.join(lines[:1] + headers)
